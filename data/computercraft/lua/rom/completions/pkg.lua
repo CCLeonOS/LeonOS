d925494346c92b698f093b2006387206c6727ff0
Leonmmcoset <testmod@qq.com>
2025-09-01 22:20:13 +0800

    fix: 更新版本号至0.3.3
    修复pkg命令的自动补全功能并同步更新所有相关文件中的版本号

--- data/computercraft/lua/rom/completions/pkg.lua
+++ data/computercraft/lua/rom/completions/pkg.lua
@@ -2,5 +2,5 @@ local shell = require("shell")
 local completion = require("cc.shell.completion")
 
 shell.setCompletionFunction("pkg", completion.build(
-  completion.choice{"install", "update", "remove", "list", "search", "info", "help"},
+
 ))
